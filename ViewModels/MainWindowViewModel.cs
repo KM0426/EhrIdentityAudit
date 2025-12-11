077ff99d8458b140227b89ab3c08559fa401ed9f
KM0426 <kamizugu@east.ncc.go.jp>
2025-12-12 08:25:24 +0900

    a

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace EhrIdentityAudit.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+    // File → Read
+    public void ReadCommand()
+    {
+        // TODO: 読み込み処理を書く
+        Debug.WriteLine("ReadCommand 実行");
+        
+        // TODO: ファイル選択ダイアログを開く処理を書く
+        // ファイルはエクセルファイルを想定
+        // ファイルは三種類のパターンがある
+        // 1. 電子カルテ利用者一覧、２．職員一覧、３．委託派遣一覧
+        
+    }
+
+    // File → Exit
+    public void ExitCommand()
+    {
+        Environment.Exit(0);
+    }
+
+    // Config → Config
+    public void ShowConfigCommand()
+    {
+        // TODO: 設定画面を開く処理
+        Debug.WriteLine("ShowConfigCommand 実行");
+    }
+}
